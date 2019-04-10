0ec87708a2c3e1580c8c42768a5186311a9af730
Achintya Paul <34968128+acpaul1912@users.noreply.github.com>
2019-04-11 02:18:17 +0530

    Sort an array of 0s, 1s and 2s
    Given an array A[] consisting 0s, 1s and 2s, write a function that sorts A[]. The functions should put all 0s first, then all 1s and all 2s in last.
    Examples:
    
    Input :  {0, 1, 2, 0, 1, 2}
    Output : {0, 0, 1, 1, 2, 2}
    
    Input :  {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}
    Output : {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}

---
+++ sample.cpp
@@ -0,0 +1,35 @@
+//Sort an array of 0s, 1s and 2s
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() 
+{ 
+   
+    vector<int> ve = {0,0,0,1,1,1,2,2,2,1,0};
+    int low = 0;
+    int pivot = 1;
+    int mid = low;
+    int high = ve.size()-1;
+
+    while(mid < high) {
+
+    	if(ve[mid] < pivot) {
+
+    		swap(ve[low], ve[mid]);
+    		low ++ ;
+    		mid++;
+    	}
+    	else if(ve[mid] > pivot) {
+    		swap(ve[mid],ve[high]);
+    		--high;
+    	}
+        else {
+        	++mid;
+        }
+   }
+
+   for(int &vec: ve){
+   	   cout<<vec;
+   }
+    return 0; 
+} 
